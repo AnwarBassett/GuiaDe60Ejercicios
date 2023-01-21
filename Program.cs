c0d28408843c1f3c5f486a8341b0b7cad00c708a
AnwarBassett <abassettma@gmail.com>
2023-01-20 20:43:15 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Ejercicio #4 Determine la raíz Cuadrada de un número cualquiera.
+                int n;
+                do
+                {
+                    Console.WriteLine("Ingrese un numero");
+                    n = int.Parse(Console.ReadLine());
+                    if (n <= 0)
+                    {
+                        Console.WriteLine("No puede agregar cero, numeros negativos o letras");
+                    }
+                } while (n<=0);
+                Console.WriteLine("La raiz cuadrada de {0} es: {1}",n,(Math.Pow(n,2)));
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("El error se encuentra en: "+ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
